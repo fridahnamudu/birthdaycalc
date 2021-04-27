f28f3493bc394a3f2e68af09a1af340f01f58451
fridahnamudu <fridah.namudu@gmail.com>
2021-04-27 14:20:02 +0300

    add changes

--- js/script.js
+++ js/script.js
@@ -8,8 +8,7 @@ var female  = ["akosua", "Adwoa", "Abenaa", "akua", "yaa", "afua", "ama"]
 function getName(){
 	//fn();
 	var date = document.getElementById("date").value;
-    var day = new Date(date).getDay();
-     
+	var day = new Date(date).getDay();
 	var ismale = document.getElementById("male").checked;
 	
 	var isfemale = document.getElementById("female").checked;
@@ -61,107 +60,4 @@ function getName(){
 }
 
 
-// function fn(){
-	
-	
-	
 
-	
-// //console.log(date)
-
-
-// 	if (day < 0 || day > 31 ) {
-// 		document.getElementsByClassName("dateerror");
-// 		return false;
-// 	}else if (date == ""){
-// 		document.getElementsByClassName("dateerror");
-//         return false;
-// 	}else{
-// 		d.setDate(date);
-// 	}
-
-// 	if (month < 0 || day > 12){
-// 		document.getElementsByClassName("montherror");
-// 		return false;
-// 	}else if (month == "") {
-// 		document.getElementsByClassName("montherror");
-// 		return false;
-// 	}else{
-// 		d.setMonth(month);
-// 	}
-
-// 	if (year < 0){
-// 		document.getElementsByClassName("yearerror");
-// 		return false;
-// 	}else if (year == ""){
-// 		document.getElementsByClassName("yearerror");
-// 		return true;
-// 	}
-// }
-
-
-
-
-
-// let dateInput = prompt("Please enter your date of birth:\n Syntax 1: YYYY-MM-DD\n Syntax 1: YYYY, MM, DD");
-// let genderInput = prompt("Please indicate your gender:\nSpecify with M or F");
-
-// let dob = new Date(dateInput);
-// let gender;
-// let akan_name;
-
-
-// const checkDayMonth = (dob) =>
-// {
-
-// 	if(dob.getDate() < 0 || dob.getDate() > 31)
-// 	{
-// 		console.error("Invalid day input!");
-// 		alert("Please re-enter the date details correctly.");
-// 	}
-
-// 	else if(dob.getMonth().toString() == "NaN")
-// 	{
-// 		console.error("Invalid month input in date!");
-// 		alert("Please re-enter the date details correctly.");
-// 	}
-
-// 	else
-// 		console.log("Details correctly entered!");
-
-// }
-
-
-
-// const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-// const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-
-
-
-// const checkGender = (genderInput) =>
-// {
-// 	if(genderInput == 'M' || genderInput == 'm' || genderInput == "MALE" || genderInput == "Male")
-// 		gender = "Male";
-// 	else if(genderInput == 'F' || genderInput == 'f' || genderInput == "FEMALE" || genderInput == "Female")
-// 		gender = "Female";
-// 	else
-// 		console.error("Incorrect input!");
-// }
-
-
-
-// checkDayMonth(dob);
-// checkGender(genderInput);
-
-
-// console.warn(dob.getDay() + " index of day of the week");
-// for(let nameIndex = 0; nameIndex < 7; nameIndex++)
-// {
-// 	if(dob.getDay() == nameIndex && gender == "Male")
-// 		akan_name = maleNames[nameIndex];
-// 	else if(dob.getDay() == nameIndex && gender == "Female")
-// 		akan_name = femaleNames[nameIndex];
-
-// }
-
-// console.log(`Your Akan name is ${akan_name}`);
